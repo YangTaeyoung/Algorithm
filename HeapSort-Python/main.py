21f8d6f5b1a731f587cc6342195dea31897c88f1
Taeyoung Yang <0130yang@gmail.com>
2021-10-07 03:15:35 +0900

    힙 정렬 파이썬 기반. 백준 2751번 문제 정답.

--- HeapSort-Python/main.py
+++ HeapSort-Python/main.py
@@ -1,45 +1,35 @@
 def make_heap(arr: list):
-    if len(arr) == 0:
-        return []
-
-    for i in range(len(arr) // 2, len(arr)):
+    for i in range(len(arr)):
         child = i
-        parent = child // 2
-
-        while True:
+        while child != 0:
+            parent = (child - 1) // 2
             if arr[parent] < arr[child]:
-                temp = arr[parent]
-                arr[parent] = arr[child]
-                arr[child] = temp
+                arr[parent], arr[child] = arr[child], arr[parent]
             child = parent
-            parent = parent // 2
-            if child <= 0:
-                break
-    return arr
+
 
 
 def heap_sort(heap: list):
-    n = len(heap)
-    for i in range(n - 1, 0, -1):
+    make_heap(heap)
+    for i in range(len(heap) - 1, 0, -1):
         #  첫 요소랑 마지막 요소 바꾸기
         root = 0
-        child = root * 2 + 1
+        child = 0
         heap[i], heap[root] = heap[root], heap[i]
 
-        while True:
-            if child >= i:
-                break
-            if heap[child] < heap[child + 1] and child + 1 < i: # 자식을 옆으로 옮겨주는 부분.
+        while child < i:
+            child = child * 2 + 1
+            if child < i-1 and heap[child] < heap[child + 1]:  # 자식을 옆으로 옮겨주는 부분.
                 child += 1
-            if heap[child] > heap[root]:  # 자식이 부모보다 큰 경우
+            if child < i and heap[child] > heap[root]:  # 자식이 부모보다 큰 경우
                 heap[child], heap[root] = heap[root], heap[child]
             root = child
-            child = child * 2 + 1
-    return heap
+
 
 arr = []
 for i in range(int(input())):
     arr.append(int(input()))
+heap_sort(arr)
 
-for num in heap_sort(make_heap(arr)):
+for num in arr:
     print(num)
